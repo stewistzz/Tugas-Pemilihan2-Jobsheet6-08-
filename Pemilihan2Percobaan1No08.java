dd14102047e727e35c56567d1629cdb7e66bcc3a
unknown <dimassetyonugroho30@gmail.com>
2023-10-16 23:12:05 +0700

    code awal tahun kabisat

---
+++ Pemilihan2Percobaan1No08.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1No08 {
+    public static void main(String[] args) {
+        Scanner Input08 = new Scanner(System.in);
+        
+        int tahun;
+
+        System.out.print("Masukkan tahun = ");
+        tahun = Input08.nextInt();
+
+        if (tahun % 4 == 0) {
+            if (tahun % 100 != 0)
+                System.out.println("tahun kabisat");
+        } else {
+            System.out.println("BUkan tahun kabisat");
+        }
+    }
+}
