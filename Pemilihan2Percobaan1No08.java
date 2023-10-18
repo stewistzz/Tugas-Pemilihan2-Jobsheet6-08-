73f13f56fbf5536fb0356d47f0c1752b8ef7eeb6
unknown <dimassetyonugroho30@gmail.com>
2023-10-18 16:34:25 +0700

    merubah agar 2100 menjadi tahun kabisat

--- Pemilihan2Percobaan1No08.java
+++ Pemilihan2Percobaan1No08.java
@@ -10,18 +10,14 @@ public class Pemilihan2Percobaan1No08 {
         tahun = Input08.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
-                System.out.println("tahun kabisat");  
+            if ((tahun % 100) != 0) {
+                System.out.println("tahun kabisat"); 
+            } else {
+                System.out.println("Tahun tersebut bukanlah tahun kabisat");
+            }
+                 
         }else {
             System.out.println("Bukan tahun kabisat");
         }
-
-
-
-        // if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
-        //     System.out.println("Tahun kabisat");
-        // } else {
-        //     System.out.println("Bukan tahun kabisat");
-        // }
     }
 }
